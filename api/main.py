a8564dc185d99c706a012ed153f7c6fae67d8fba
Jeevan Sai V <jeevansaiv2007@gmail.com>
2026-08-22 00:11:44 +0530

    Update project

--- api/main.py
+++ api/main.py
@@ -46,10 +46,7 @@ app = FastAPI(title="Procurement Agent API", lifespan=lifespan)
 # than hardcoding the one port it happened to start on last.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", "http://127.0.0.1:5173",
-        "http://localhost:5174", "http://127.0.0.1:5174",
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
